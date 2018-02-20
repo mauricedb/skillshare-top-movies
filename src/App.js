4e9824ffe52849bd73ae891329745bfe1c78fc17
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-02-20 10:16:33 +0100

    State management

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 
 import MovieCard from './components/MovieCard';
 
-const movies = [
+const originalMovies = [
   { id: 1, title: 'Star Wars' },
   { id: 2, title: 'Star Trek' },
   { id: 3, title: 'Blade Runner' }
 ];
 
 class App extends Component {
+  state = { movies: [] };
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ movies: originalMovies }), 5000);
+  }
+
   render() {
+    const { movies } = this.state;
+
     return (
       <div className="App">
         {movies.map(movie => <MovieCard key={movie.id} movie={movie} />)}
