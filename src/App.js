efb1db8c1ccd13b6909609e227b5e78ab6d997f4
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-03-17 12:03:07 +0100

    Searching for movies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
+import Input, { InputAdornment } from 'material-ui/Input';
 
 import './App.css';
 import apiKey from './apiKey';
@@ -18,10 +18,24 @@ const styles = {
 };
 
 class App extends Component {
-  state = { movies: [], selectedMovie: null };
+  state = { movies: [], selectedMovie: null, searchText: '' };
 
   selectMovie = movie => this.setState({ selectedMovie: movie });
   clearMovie = () => this.setState({ selectedMovie: null });
+
+  searchTextChanged = e => this.setState({ searchText: e.target.value });
+
+  search = async e => {
+    e.preventDefault();
+
+    const { searchText } = this.state;
+
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchText}`
+    );
+    const json = await response.json();
+    this.setState({ movies: json.results });
+  };
 
   async componentDidMount() {
     const response = await fetch(
@@ -32,15 +46,29 @@ class App extends Component {
   }
 
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, searchText } = this.state;
 
     return (
       <div>
         <AppBar position="fixed">
           <Toolbar>
-            <Typography variant="title" color="inherit">
+            <Typography variant="title" color="inherit" className="title">
               Top Movies
             </Typography>
+            <form onSubmit={this.search}>
+              <Input
+                type="search"
+                value={searchText}
+                onChange={this.searchTextChanged}
+                startAdornment={
+                  <InputAdornment>
+                    <span role="img" aria-label="Search">
+                      🔍
+                    </span>
+                  </InputAdornment>
+                }
+              />
+            </form>
           </Toolbar>
         </AppBar>
         <div className="movies">
